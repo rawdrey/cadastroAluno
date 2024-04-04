7f0c7bc7c38fab660f4eabc73ff68ed6048ca230
Rawdrey <rawdrey@gmail.com>
2024-04-04 09:23:25 -0300

    implementado class Repositório

---
+++ cadastroAluno/Repositorio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastroAluno
+{
+    public static class Repositorio
+    {
+        private static int id = 1;
+        private static Aluno[] alunos = new Aluno[100];
+        private static int indice = 0;
+
+        public void AdcionarAluno(string nome, string turma)
+        {
+            aluno aluno = newAluno(id, nome, turma);
+            alunos[indice] = aluno;
+            indice++;
+            id++;
+        }
+
+
+    }
+}
